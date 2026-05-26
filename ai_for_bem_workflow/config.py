b9d531470de9e681ed3be53e6cabb87eaf6219de
Hussein Elehwany <husseinelehwany@cmail.carleton.ca>
2026-05-26 12:23:34 -0400

    update energyplus version

--- ai_for_bem_workflow/config.py
+++ ai_for_bem_workflow/config.py
@@ -4,6 +4,6 @@ import sys
 if sys.platform == "win32":
     EPLUS_DIR = r"C:\EnergyPlusV24-1-0"
 else:
-    EPLUS_DIR = "/usr/local/EnergyPlus-24-1-0"
+    EPLUS_DIR = "/usr/local/EnergyPlus-26-1-0"
 
 EPLUS_IDD = os.path.join(EPLUS_DIR, "Energy+.idd")
