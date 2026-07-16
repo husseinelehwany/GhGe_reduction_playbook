fd8c5fe838fe5367fd67fef708ab3b52ce4a57c7
Hussein Elehwany <husseinelehwany@cmail.carleton.ca>
2026-07-16 18:26:36 -0400

    upgraded to EnergyPlus v26.1

--- ai_for_bem_workflow/config.py
+++ ai_for_bem_workflow/config.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if sys.platform == "win32":
-    EPLUS_DIR = r"C:\EnergyPlusV24-1-0"
+    EPLUS_DIR = r"C:\EnergyPlusV26-1-0"
 else:
     EPLUS_DIR = "/usr/local/EnergyPlus-26-1-0"
 
